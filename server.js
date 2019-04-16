4f40d7969a205ee92161c40ef4b39c9464a2d01b
Ilja Winokurow <Ilja.Winokurow@clearit.local>
2019-04-16 12:37:12 +0200

    Fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/cleanmanager'));
 
 app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/app/dist/cleanmanager/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cleanmanager/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
