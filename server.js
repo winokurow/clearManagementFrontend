1c0b87a5678dbce0e8b5e758f2e96c4d102abc1e
Ilja Winokurow <Ilja.Winokurow@clearit.local>
2019-04-16 12:22:57 +0200

    Outputh Path is changed

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/cleanmanager'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/cleanmanager/index.html'));
+app.get('/*', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/dist/cleanmanager/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
