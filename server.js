585b963a9fbfaeb8f8f9077814eaa91233144100
Ilja Winokurow <Ilja.Winokurow@clearit.local>
2019-04-16 12:41:39 +0200

    Fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cleanmanager'));
+app.use(express.static(__dirname + '/dist/clean-web'));
 
 app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/dist/cleanmanager/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/clean-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
